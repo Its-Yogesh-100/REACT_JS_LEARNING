6deb31f405ab9b31fbc81d027807736f2fde5f88
YOGESH KAUSHIK <virus2.0.exe@gmail.com>
2023-06-13 16:22:45 +0530

    react app

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <div class="App-header">
+        Header
+      </div>
+    
+       Hello World
+       
+    </div>
+  );
+}
+
+export default App;
